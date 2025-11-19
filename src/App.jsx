1934af3bd1c1f1527095ae0a6a936d4111ff55b1
ajaymakireddy <ajaymakireddy@gmail.com>
2025-11-19 15:25:02 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
 
   useEffect(() => navigate("/login"), []);
   // useEffect(() => navigate('/admin') , [])
+  console.log("Something");
 
   const [role, setRole] = useState();
 
